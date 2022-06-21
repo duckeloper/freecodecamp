e81f313ab020a70a75655c13ed30852a99859157
Byungchan Park <duckeloper@gmail.com>
2022-06-21 13:36:46 +0900

    git error fix

---
+++ javascript/basic-js/basic.js
@@ -0,0 +1,24 @@
+// This is an in-line comment.
+
+/* This is a
+multi-line comment */
+
+// Declare JavaScript Variables
+
+var myName;
+
+// Storing Values with the Assignment Operator
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+// Assigning the Value of One Variable to Another
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
