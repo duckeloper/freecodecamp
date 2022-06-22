e4e7f0484b5466ec65131034f877bf2d0d08801b
Byungchan Park <duckeloper@gmail.com>
2022-06-22 13:41:14 +0900

    praciticing javascript

--- javascript/basic-js/basic.js
+++ javascript/basic-js/basic.js
@@ -8,17 +8,73 @@ multi-line comment */
 var myName;
 
 // Storing Values with the Assignment Operator
-// Setup
 var a;
-
-// Only change code below this line
 a = 7;
 
 // Assigning the Value of One Variable to Another
-// Setup
 var a;
 a = 7;
 var b;
-
-// Only change code below this line
 b = a;
+
+// Initializing Variables with the Assignment Operator
+var a = 9;
+
+// Declare String Variables
+var myFirstName = "Byungchan";
+var myLastName = "Park";
+
+// Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Understanding Case Sensitivity in Variables
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// Declare a Read-Only Variable with the const Keyword
+// const: 재선언 불가, 재할당 불가
+// let: 재선언 불가, 재할당 가능
+// var: 재선언 가능, 재할당 가능
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+// Add Two Numbers with JavaScript
+const sum = 10 + 10;
+console.log(sum);
+
+// Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+
+// Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+
+// Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+// Increment a Number with JavaScript
+let myVarIncrement = 87;
+myVarIncrement++;
+
+// Decrement a Number with JavaScript
+let myVarDecrement = 11;
+myVarDecrement--;
+
