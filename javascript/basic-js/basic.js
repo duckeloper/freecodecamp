d0aeda868573b27d8b5221fbc368861c79eaee0a
Byungchan Park <duckeloper@gmail.com>
2022-06-23 13:42:33 +0900

    javascript practice

--- javascript/basic-js/basic.js
+++ javascript/basic-js/basic.js
@@ -5,76 +5,145 @@ multi-line comment */
 
 // Declare JavaScript Variables
 
-var myName;
+// var myName;
 
 // Storing Values with the Assignment Operator
-var a;
-a = 7;
+// var a;
+// a = 7;
 
 // Assigning the Value of One Variable to Another
-var a;
-a = 7;
-var b;
-b = a;
+// var a;
+// a = 7;
+// var b;
+// b = a;
 
 // Initializing Variables with the Assignment Operator
-var a = 9;
+// var a = 9;
 
 // Declare String Variables
-var myFirstName = "Byungchan";
-var myLastName = "Park";
+// var myFirstName = "Byungchan";
+// var myLastName = "Park";
 
 // Understanding Uninitialized Variables
-var a = 5;
-var b = 10;
-var c = "I am a";
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
 
 // Understanding Case Sensitivity in Variables
 // Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
 // Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
 
 // Explore Differences Between the var and let Keywords
-let catName = "Oliver";
-let catSound = "Meow!";
+// let catName = "Oliver";
+// let catSound = "Meow!";
 
 // Declare a Read-Only Variable with the const Keyword
 // const: 재선언 불가, 재할당 불가
 // let: 재선언 불가, 재할당 가능
 // var: 재선언 가능, 재할당 가능
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+// const FCC = "freeCodeCamp"; // Change this line
+// let fact = "is cool!"; // Change this line
+// fact = "is awesome!";
+// console.log(FCC, fact); // Change this line
 
 // Add Two Numbers with JavaScript
-const sum = 10 + 10;
-console.log(sum);
+// const sum = 10 + 10;
+// console.log(sum);
 
 // Subtract One Number from Another with JavaScript
-const difference = 45 - 33;
+// const difference = 45 - 33;
 
 // Multiply Two Numbers with JavaScript
-const product = 8 * 10;
+// const product = 8 * 10;
 
 // Divide One Number by Another with JavaScript
-const quotient = 66 / 33;
+// const quotient = 66 / 33;
 
 // Increment a Number with JavaScript
-let myVarIncrement = 87;
-myVarIncrement++;
+// let myVarIncrement = 87;
+// myVarIncrement++;
+
 
 // Decrement a Number with JavaScript
-let myVarDecrement = 11;
-myVarDecrement--;
+// let myVarDecrement = 11;
+// myVarDecrement--;
+
+
+// Create Decimal Numbers with JavaScript
+// const ourDecimal = 5.7;
+// const myDecimal = 1.14;
+
+
+// Multiply Two Decimals with JavaScript
+// const product = 2.0 * 2.5;
+
+
+// Divide One Decimal by Another with JavaScript
+// const quotient = 4.4 / 2.0;
+
+
+// Finding a Remainder in JavaScript
+// const remainder = 11 % 3;
+
+// Compound Assignment With Augmented Addition
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// a += 12;
+// b += 9;
+// c += 7;
+
+
+// Compound Assignment With Augmented Subtraction
+// let a = 11;
+// let b = 9;
+// let c = 3;
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+// Compound Assignment With Augmented Multiplication
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// Compound Assignment With Augmented Division
+// let a = 48;
+// let b = 108;
+// let c = 33;
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
+// Escaping Literal Quotes in Strings
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // 문자열에 따옴표 넣고 싶으면 백슬래시
+
+// Quoting Strings with Single Quotes
+// const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Escape Sequences in Strings
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \r	carriage return
+// \t	tab
+// \b	word boundary
+// \f	form feed
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 
