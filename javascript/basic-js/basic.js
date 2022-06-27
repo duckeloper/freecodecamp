3d7fa07b3144b9f53a4d87e6754855c385745c3e
Byungchan Park <duckeloper@gmail.com>
2022-06-27 14:18:26 +0900

    javascript syntax practice

--- javascript/basic-js/basic.js
+++ javascript/basic-js/basic.js
@@ -743,3 +743,356 @@ caseInSwitch(1); */
 }
 
 switchOfStuff(1); */
+
+
+
+
+// Multiple Identical Options in Switch Statements
+/* function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1); */
+
+
+
+
+// Replacing If Else Chains with Switch
+/* function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7); */
+
+
+
+
+// Returning Boolean Values from Functions
+/* function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+isLess(10, 15); */
+
+
+
+
+// Return Early Pattern for Functions
+/* // Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2); */
+
+
+
+
+// Counting Cards
+/* let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+      break;
+  }
+
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  };
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A'); */
+
+
+
+
+// Build JavaScript Objects
+/* const myDog = {
+  // Only change code below this line
+  "name": "Doggy",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Meow"]
+
+  // Only change code above this line
+}; */
+
+
+
+
+// Accessing Object Properties with Dot Notation
+/* // Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line */
+
+
+
+
+// Accessing Object Properties with Bracket Notation
+/* // Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line */
+
+
+
+
+// Accessing Object Properties with Variables
+/* // Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+ */
+
+
+
+
+// Updating Object Properties
+/* // Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder"; */
+
+
+
+
+// Add New Properties to a JavaScript Object
+/* const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof"; */
+
+
+
+
+// Delete Properties from a JavaScript Object
+/* // Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails; */
+
+
+
+
+// Using Objects for Lookups
+/* // Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie"); */
+
+
+
+
+// Testing Objects for Properties
+/* function checkObj(obj, checkProp) {
+  // Only change code below this line
+  console.log("prop: " + checkProp);
+  console.log("property t/f: " + obj.hasOwnProperty(checkProp))
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+} */
+
+
+
+
+// Manipulating Complex Objects
+/* const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Crush",
+    "title": "NAPPA",
+    "release_year": 2020,
+    "formats": [
+      "CD",
+      "digital"
+    ]
+  }
+]; */
+
+
+
+
+// Accessing Nested Objects
+/* const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"]; */
+
+
+
+
+// Accessing Nested Arrays
+/* const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1]; */
+
+
+
+
+// Record Collection
